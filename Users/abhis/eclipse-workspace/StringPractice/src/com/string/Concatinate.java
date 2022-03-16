5078edf89ca89db77d0f96ed546d645fe7cf9526
abhishek <guptabhishek0714@gmail.com>
2022-03-16 12:36:06 +0530

    update file string

---
+++ Users/abhis/eclipse-workspace/StringPractice/src/com/string/Concatinate.java
@@ -0,0 +1,21 @@
+package com.string;
+
+public class Concatinate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str1="abc";
+		String str2="xyx";
+		String str3="gfd";
+		
+		String s1=str3.concat(str1.concat(str2));
+		
+		
+		//String add=s1.concat(str3);
+		System.out.println(s1);
+		
+
+	}
+
+}
